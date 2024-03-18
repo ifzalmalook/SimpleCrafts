8624f23150c24291f36f92e4524b1d973781dfb7
ifzal malook <ifzal.malook@gmail.com>
2024-03-18 20:31:15 +0000

    Create Projects model and import relevant fields

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from djrichtextfield.models import RichTextField
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Project(models.Model):
+    author = models.ForeignKey(User, related_name='project_author', on_delete=models.CASCADE)
+    title = models.CharField (max_length=255, unique=True, blank=False, null=False)
+    slug = models.SlugField (max_length=255, unique=True, null=False)
+    description = models.CharField (max_length=600, blank=False, null=False)
+    materials = RichTextField(max_length=10000, blank=False, null=False)
+    steps = RichTextField(max_length=10000, blank=False, null=False)
+    image = CloudinaryField ('image', default='placeholder')
+    published_on = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, null=False, blank=False, on_delete=models.CASCADE)
