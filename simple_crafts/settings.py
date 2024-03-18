1dd61962913ffd45077ce834e83b2e5bc8bbb36b
ifzal malook <ifzal.malook@gmail.com>
2024-03-18 20:20:13 +0000

    Remove unecessary icons from footer

--- simple_crafts/settings.py
+++ simple_crafts/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-ifzalmalook-simplecraft-aoxd1419g49.ws-eu110.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ifzalmalook-simplecraft-itue9ytg8nf.ws-eu110.gitpod.io', '.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io', 'https://*.herokuapp.com']
 
 
