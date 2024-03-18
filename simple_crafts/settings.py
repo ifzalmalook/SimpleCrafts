c18035562c2bf6c4687726835abac139996cf5fe
ifzal malook <ifzal.malook@gmail.com>
2024-03-18 20:47:46 +0000

    Debug to false

--- simple_crafts/settings.py
+++ simple_crafts/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ifzalmalook-simplecraft-itue9ytg8nf.ws-eu110.gitpod.io', '.herokuapp.com']
 CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io', 'https://*.herokuapp.com']
