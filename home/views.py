376b21d7d2b1050a9b078d40698b8817f1ee54b7
ifzal malook <ifzal.malook@gmail.com>
2024-03-21 20:19:07 +0000

    Add if else statement to homepage to change content for logged in users

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.contrib.auth import logout
 from projects.models import Project
@@ -6,14 +6,15 @@ from django.db.models import Count
 
 # Create your views here.
 
+
 class Home(ListView):
     template_name = 'home/home.html'
     model = Project
 
-    # Display most liked projects on home page
+    #  Display most liked projects on home page    
     def get_queryset(self):
-        return Project.objects.annotate(num_likes=Count('likes')).order_by('-num_likes')[:3]
+         return Project.objects.annotate(num_likes=Count('likes')).order_by('-num_likes')[:3]
 
-    # def get_queryset(self):
-    #     return self.model.objects.all()[:3]
+    def get_queryset(self):
+        return self.model.objects.all()[:3]
 
