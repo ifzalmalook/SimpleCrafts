dd432f93f628539ba60e44f088b882b4c2805730
ifzal malook <ifzal.malook@gmail.com>
2024-03-21 16:12:15 +0000

    Add logo

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.contrib.auth import logout
 from projects.models import Project
+from django.db.models import Count
 
 # Create your views here.
 
@@ -9,6 +10,10 @@ class Home(ListView):
     template_name = 'home/home.html'
     model = Project
 
+    # Display most liked projects on home page
     def get_queryset(self):
-        return self.model.objects.all()[:3]
+        return Project.objects.annotate(num_likes=Count('likes')).order_by('-num_likes')[:3]
+
+    # def get_queryset(self):
+    #     return self.model.objects.all()[:3]
 
